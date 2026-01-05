a30edda9be001bebd4ee327c6e4db90297afc3e4
Дамјан Георгиевски <gdamjan@gmail.com>
2026-01-05 20:34:20 +0100

    fix formating

--- src/demo/__init__.py
+++ src/demo/__init__.py
@@ -1,13 +1,16 @@
 import boto3
 
-import os, sys
+import os
+import sys
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
+
 def main() -> None:
     print("Hello from main()!")
+
 
 def lambda_handler(event, context):
     print("print from lambda_handler() to stderr", file=sys.stderr)
